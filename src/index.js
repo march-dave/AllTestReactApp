6340400096542eda99cd2135edaea3a1df882c5d
march-dave <ysangy@gmail.com>
2017-01-29 15:07:12 -0500

    Redux Start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+// Action
+const INCREMENT = 'INCREMENT';
+function increase(diff) {
+  return {
+    type: INCREMENT,
+    addBy: diff
+  };
+}
+
+// Reducer
+const initialState = {
+  value: 0
+};
+
+const counterReducer = (state=initialState, action) => {
+  switch(action.type) {
+      case INCREMENT: 
+        return Object.assign({}, state, {
+          value: state.value + action.addBy
+        });
+        default:
+          return state;
+  }
+}
+
+// Store
+const store = createStore(counterReducer);
+
+
+
+ReactDOM.render(<
+    App /> ,
+    document.getElementById('root')
 );
